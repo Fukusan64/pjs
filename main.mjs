947b97b0a873ccf4e5ba0f156cd1ec6b81390268
Fukusan64 <ikishikawa64@gmail.com>
2024-10-07 21:41:03 +0900

    説明を修正

--- main.mjs
+++ main.mjs
@@ -40,7 +40,7 @@ if (!command) {
       '',
       'utils',
       '    [TOPIC_REFERENCE]line',
-      '        Number of rows currently being processed (0 indexed). " -a" option is specified, the number of input rows is returned.',
+      '        Number of lines currently being processed (0 indexed). If the " -a " option is specified, the number of input lines.',
       '    p',
       '        const p = (data) => {console.log(data);return data;}',
       '    e',
